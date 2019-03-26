148817425126794c29a07ccb9901cd94984e4acf
Sebastian Horton <36234258+SebastianH11@users.noreply.github.com>
2019-03-26 13:49:52 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _312551U2Summative
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Contact contact = new Contact();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonUpdateInfo_Click(object sender, RoutedEventArgs e)
+        {
+            contact.GetAge();
+            UpdateInfo();
+        }
+        public void UpdateInfo()
+        {
+            if (((bool)rbFirstName.IsChecked) == true)
+            {
+                contact.firstName = txtbxInput.Text;
+            }
+            if (((bool)rbLastName.IsChecked) == true)
+            {
+                contact.lastName = txtbxInput.Text;
+            }
+            lblOutput.Text = "First Name: " + contact.firstName + "\r Last Name: " + contact.lastName + 
+                "\r Age: " + contact.age + "\r Email: " + contact.email;
+        }
+    }
+    class Contact
+    {
+        public string firstName;
+        public string lastName;
+        public DateTime birthday = new DateTime(2002,6,2);
+        DateTime currentDate = DateTime.Now;
+        public string email;
+        public int age;
+
+        public void ReadFromFile()
+        {
+
+        }
+        public void WriteToFile()
+        {
+
+        }
+        public int GetAge()
+        {
+            age = (currentDate.Year - birthday.Year);
+            Console.WriteLine(age.ToString());
+            return age;
+        }
+    }
+}
