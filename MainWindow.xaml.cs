4b93e276cf9b582eb588b881ff79d6362ba5a741
Sebastian Horton <36234258+SebastianH11@users.noreply.github.com>
2019-03-28 22:52:41 -0400

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.ComponentModel;
+using System.IO;
 
 namespace _312551U2Summative
 {
@@ -21,116 +23,150 @@ namespace _312551U2Summative
     /// </summary>
     public partial class MainWindow : Window
     {
-
         Contact contact;
-        string firstName;
-        string lastName;
-        DateTime birthday;
-        string email;
+
         public MainWindow()
         {
-            
+
             InitializeComponent();
-            contact = new Contact(firstName, lastName, birthday, email);
-            contact.ReadFromFile();
+
+            contact = new Contact();
+            ///checks if the user's "contact.txt" file exists, if it doesnt it creates one and adds example info to it
+            if (!File.Exists("contact.txt")) 
+            {
+               
+                contact.WriteToFile();
+                contact.ReadFromFile(txtbxFirstName, txtbxLastName, txtbxBirthday, txtbxEmail); 
+            }
+            else
+                contact.ReadFromFile(txtbxFirstName, txtbxLastName, txtbxBirthday, txtbxEmail);
         }
 
-        private void ButtonSave_Click(object sender, RoutedEventArgs e)
-        {
-            GetInfo();
-            //contact.WriteToFile();
-        }
-        public void GetInfo()
-        {
-            firstName = txtbxFirstName.Text;
-
-            lastName = txtbxLastName.Text;
-
-            bool isNumber = DateTime.TryParse(txtbxBirthday.Text, out birthday);
-
-            if (isNumber == false)
-                MessageBox.Show("Please enter a birthday in YYYY/MM/DD format.");
-
-            email = txtbxEmail.Text;
-            Console.WriteLine(firstName + lastName + birthday + email);
-        }
-
-        
         void MainWindow_Closing(object sender, CancelEventArgs e)
         {
-            contact.WriteToFile(firstName, lastName, birthday, email);
-            Console.WriteLine(firstName +  lastName + birthday + email);
+            contact.getContact(txtbxFirstName, txtbxLastName, txtbxBirthday, txtbxEmail, e);
+            contact.WriteToFile();
         }
 
         private void BtnDisplayAge_Click(object sender, RoutedEventArgs e)
         {
-
+            contact.getBirthday(txtbxBirthday);
+            contact.getAge(txtblOutput);
         }
     }
     class Contact
     {
-        private string _firstName;
-        private string _lastName;
-        private DateTime _birthday;
-        private string _email;
-        DateTime currentDate = DateTime.Now;
-        public int age;
-        
-        public Contact(string firstName, string lastName, DateTime birthday, string email)
+        ///declared memeber variables
+        private string _firstName = "Sebastian";
+        private string _lastName  = "Horton";
+        private DateTime _birthday = new DateTime(2002,6,2);
+        private string _email = "sebastian@hobro.ca";
+        private int age;
+
+        public void getContact(TextBox firstName, TextBox lastName, TextBox birthday, TextBox email, CancelEventArgs e)
         {
-            _firstName = firstName;
-            _lastName = lastName;
-            _birthday = birthday;
-            _email = email;
+           
+            ///overriding the stored information with the user's input
+            if (!firstName.Text.Contains(",")) ///checks for inappropriately placed commas
+            {
+                _firstName = firstName.Text;
+                _lastName = lastName.Text;
+            }
+            else
+            {
+               MessageBoxResult mbr = MessageBox.Show("Your name cannot contain a comma.", "warning", MessageBoxButton.OK);
+                cancelClosing(e, mbr);
+            }
+
+            bool isNumber = DateTime.TryParse(birthday.Text, out _birthday); ///checks for a valid birthday in YYYY/MM/DD
+            if (isNumber == false)
+            {
+                MessageBoxResult mbr = MessageBox.Show("Please enter a birthday in YYYY/MM/DD format.", "warning", MessageBoxButton.OK);
+                cancelClosing(e, mbr);
+            }
+
+            if (email.Text.Contains("@")) ///checks for valid a email adress
+                _email = email.Text;
+            else
+            {
+                MessageBoxResult mbr = MessageBox.Show("Please enter a valid email adress.", "warning", MessageBoxButton.OK);
+                cancelClosing(e, mbr);
+            }
         }
-        public void ReadFromFile()
+
+        private static void cancelClosing(CancelEventArgs e,MessageBoxResult mbr)
+        {
+            if (mbr == MessageBoxResult.OK)
+                e.Cancel = true;
+        }
+
+        public void getBirthday(TextBox birthday)
+        {
+            bool isNumber = DateTime.TryParse(birthday.Text, out _birthday); ///checks for a valid birthday in YYYY/MM/DD
+            if (isNumber == false)
+                MessageBox.Show("Please enter a birthday in YYYY/MM/DD format.");
+        }
+
+        public void ReadFromFile(TextBox firstName, TextBox lastName, TextBox birthday, TextBox email)
+        {
+                try
+                {
+                    ///taking the stored information from the text file and converting it into variables
+                    System.IO.StreamReader sr = new System.IO.StreamReader("contact.txt");
+                    string s = sr.ReadLine(); ///temportary string containg all of the information
+
+                    _firstName = s.Substring(0, s.IndexOf(",")); ///adding the firstName to the _firstName private variable
+                    s = s.Substring(s.IndexOf(",") + 1); ///removing the firstName information from the string
+
+                    _lastName = s.Substring(0, s.IndexOf(",")); ///adding the lastName to the _lastName private variable
+                    s = s.Substring(s.IndexOf(",") + 1); ///removing the lastName information from the string
+
+                    DateTime.TryParse(s.Substring(0, s.IndexOf(",")), out _birthday); ///adding the birthday to the _birthday private variable
+                    s = s.Substring(s.IndexOf(",") + 1); ///removing the birthday information from the string
+
+                    _email = s.Substring(s.IndexOf(",") + 1); ///adding the only remaining information (the email) to the _email private variable 
+
+                    sr.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                ///adding the stored information into the text fields
+                firstName.Text = _firstName;
+                lastName.Text = _lastName;
+                birthday.Text = _birthday.ToShortDateString(); ///converting the _birthday into shortDate to remove the time
+                email.Text = _email;
+        }
+
+        public void WriteToFile()
         {
             try
             {
-                System.IO.StreamReader sr = new System.IO.StreamReader("contact.txt");
-                string s = sr.ReadLine();
-                _firstName = s.Substring(0, s.IndexOf(","));
-                s = s.Substring(s.IndexOf(",") + 1);
-                _lastName = s.Substring(0, s.IndexOf(","));
-                s = s.Substring(s.IndexOf(",") + 1);
-                int.TryParse(s.Substring(s.IndexOf(",") + 1), out age);
-                s = s.Substring(s.IndexOf(",") + 1);
-                _email = s.Substring(s.IndexOf(",") + 1);
-                sr.Close();
-                Console.WriteLine(_firstName + _lastName + _email + age);
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            
-        }
-        public void WriteToFile(string firstName, string lastName, DateTime birthday, string email)
-        {
-            try
-            {
+                ///writing the user's input information into the text file
                 System.IO.StreamWriter sw = new System.IO.StreamWriter("contact.txt");
-                sw.WriteLine(firstName + "," + lastName + "," + age + "," + email);
+                sw.WriteLine(_firstName + "," + _lastName + "," + _birthday + "," + _email);
                 sw.Flush();
                 sw.Close();
-                Console.WriteLine(_firstName + _lastName + age + _email);
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString());
             }
         }
-        public int GetAge()
-        {
-            if ((currentDate.DayOfYear - _birthday.DayOfYear) < 0)
-            {
-                age = (currentDate.Year - _birthday.Year - 1);
-            }
-            else
-                age = (currentDate.Year - _birthday.Year);
-            return age;
-        }
 
+        public void getAge(TextBlock txtbl)
+        {
+            DateTime currentDate = DateTime.Now;
+            ///checks if the user's birthday has occured yet this year
+            if ((currentDate.DayOfYear - _birthday.DayOfYear) < 0) ///if the user's birthday hasn't happened this year before the current date, the difference will be negative
+                age = (currentDate.Year - _birthday.Year) - 1;
+            else
+                age = currentDate.Year - _birthday.Year; ///if the user's birthday has happened this year before the current date, the difference will be positive
+            if(age <= 116)
+            txtbl.Text = "Age: " + age.ToString();
+        }
     }
 }
 
